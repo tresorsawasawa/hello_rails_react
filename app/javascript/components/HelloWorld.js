bb77c26ba70450672d3331d0f500c1bb71b6588f
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-14 03:19:08 +0200

    Add the react and redux logic

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,16 +1,23 @@
-import React from "react"
-import PropTypes from "prop-types"
-class HelloWorld extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        Message: {this.props.message}
-      </React.Fragment>
-    );
-  }
+import React from "react";
+import { useSelector, useDispatch } from 'react-redux';
+
+import { getHelloDispatcher } from "./helloworld/hello";
+
+const HelloWorld = () => {
+  const dispatch = useDispatch();
+
+  const greet =  useSelector((state) => state.helloReducer.greeting || '');
+
+  const handleClick = () => {
+    dispatch(getHelloDispatcher());
+  };
+
+  return (
+    <React.Fragment>
+      <h1>Greeting: {greet}</h1>
+      <button className="helloBtn" onClick={handleClick}>Get Greeting Randomnly</button>
+    </React.Fragment>
+  );
 }
 
-HelloWorld.propTypes = {
-  message: PropTypes.string
-};
 export default HelloWorld
